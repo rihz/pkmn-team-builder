399040fb1637ded315775e5b6790d3a02136c72a
Joshua Shields <joshuashields@gdcit.com>
2019-12-09 15:00:26 -0500

    SEEDING DONE
    SEEDING DONE

--- PkmnTeamBuilder/PkmnTeamBuilder.Data/Seed/PokemonSeed.cs
+++ PkmnTeamBuilder/PkmnTeamBuilder.Data/Seed/PokemonSeed.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PkmnTeamBuilder.Entities;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 
 namespace PkmnTeamBuilder.Data.Seed
 {
@@ -20,90 +18,80 @@ namespace PkmnTeamBuilder.Data.Seed
             var fullAbilities = SeedHelper.ReadStream<PokemonAbilities>(abilityResource);
             IEnumerable<Pokemon> pokemon = null;
 
-            try
+            pokemon = fullPokemon
+            .Select((x, index) => new Pokemon
             {
-                pokemon = fullPokemon
-                .Select((x, index) => new Pokemon
-                {
-                    BaseAtk = x.BaseAtk,
-                    BaseDef = x.BaseDef,
-                    BaseHP = x.BaseHP,
-                    BaseSpAtk = x.BaseSpAtk,
-                    BaseSpDef = x.BaseSpDef,
-                    BaseSpe = x.BaseSpe,
-                    Icon = x.Icon,
-                    Id = index + 1,
-                    Image = x.Image,
-                    Name = x.Name,
-                    Number = x.Number,
-                    Shiny = x.Shiny,
-                    Type1Id = types.First(y => y.Name.ToLower() == x.Type1.ToLower()).Id,
-                    Type2Id = x.Type2 != null ? types.First(y => y.Name.ToLower() == x.Type2.ToLower()).Id : null as int?
-                });
-            }
-            catch(Exception ex)
-            {
-                Debugger.Launch();
-                var l = 'l';
-            }
+                BaseAtk = x.BaseAtk,
+                BaseDef = x.BaseDef,
+                BaseHP = x.BaseHP,
+                BaseSpAtk = x.BaseSpAtk,
+                BaseSpDef = x.BaseSpDef,
+                BaseSpe = x.BaseSpe,
+                Icon = x.Icon,
+                Id = index + 1,
+                Image = x.Image,
+                Name = x.Name,
+                Number = x.Number,
+                Shiny = x.Shiny,
+                Type1Id = types.First(y => y.Name.ToLower() == x.Type1.ToLower()).Id,
+                Type2Id = x.Type2 != null ? types.First(y => y.Name.ToLower() == x.Type2.ToLower()).Id : null as int?
+            });
 
             builder.Entity<Pokemon>()
                 .HasData(pokemon.ToArray());
 
-            var levelUps = new List<PokemonMoveset>();
-            var tms = new List<PokemonMoveset>();
-            var trs = new List<PokemonMoveset>();
-            var eggs = new List<PokemonMoveset>();
-            var tutors = new List<PokemonMoveset>();
-
             var abilityset = new List<PokemonAbilityset>();
+            var moveset = new List<PokemonMoveset>();
 
             var moveCounter = 1;
             var abilityCounter = 1;
 
-            foreach(var pkmn in fullPokemon)
+            foreach (var pkmn in fullPokemon)
             {
-                levelUps.AddRange(CreateMoveset(pkmn.Moves.LevelUp, moves, moveTypes, "Level Up", pkmn.Id, moveCounter++));
-                tms.AddRange(CreateMoveset(pkmn.Moves.TM, moves, moveTypes, "TM", pkmn.Id, moveCounter++));
-                trs.AddRange(CreateMoveset(pkmn.Moves.TR, moves, moveTypes, "TR", pkmn.Id, moveCounter++));
-                eggs.AddRange(CreateMoveset(pkmn.Moves.Egg, moves, moveTypes, "Egg", pkmn.Id, moveCounter++));
-                tutors.AddRange(CreateMoveset(pkmn.Moves.Tutor, moves, moveTypes, "Tutor", pkmn.Id, moveCounter++));
+                var id = pokemon.First(x => x.Name == pkmn.Name).Id;
+
+                moveset.AddRange(CreateMoveset(pkmn.Moves.LevelUp, moves, moveTypes, "Level Up", id, ref moveCounter));
+                moveset.AddRange(CreateMoveset(pkmn.Moves.TM, moves, moveTypes, "TM", id, ref moveCounter));
+                moveset.AddRange(CreateMoveset(pkmn.Moves.TR, moves, moveTypes, "TR", id, ref moveCounter));
+                moveset.AddRange(CreateMoveset(pkmn.Moves.Egg, moves, moveTypes, "Egg", id, ref moveCounter));
+                moveset.AddRange(CreateMoveset(pkmn.Moves.Tutor, moves, moveTypes, "Tutor", id, ref moveCounter));
 
                 var abils = fullAbilities.First(x => x.PokemonName.ToLower() == pkmn.Name.ToLower());
 
-                foreach(var abil in abils.AbilityNames)
+                foreach (var abil in abils.AbilityNames)
                 {
                     abilityset.Add(new PokemonAbilityset()
                     {
                         Id = abilityCounter++,
                         AbilityId = abilities.First(x => x.Name.ToLower() == abil.ToLower()).Id,
-                        PokemonId = pkmn.Id
+                        PokemonId = id
                     });
                 }
             }
 
             builder.Entity<PokemonMoveset>()
-                .HasData(
-                levelUps.ToArray(),
-                tms.ToArray(),
-                trs.ToArray(),
-                eggs.ToArray(),
-                tutors.ToArray()
-            );
+                .HasData(moveset.ToArray());
 
             builder.Entity<PokemonAbilityset>()
                 .HasData(abilityset.ToArray());
         }
 
-        static IEnumerable<PokemonMoveset> CreateMoveset(string[] moveNames, List<PokemonMove> moves, List<PokemonMoveType> moveTypes, string name, int pokemonId, int id)
+        static IEnumerable<PokemonMoveset> CreateMoveset(string[] moveNames, List<PokemonMove> moves, List<PokemonMoveType> moveTypes, string name, int pokemonId, ref int id)
         {
-            return moveNames.Select((x, index) => new PokemonMoveset
+            var moveset = new List<PokemonMoveset>();
+
+            foreach(var move in moveNames)
             {
-                Id = id,
-                MoveId = moves.First(y => y.Name.ToLower() == x.ToLower()).Id,
-                MoveTypeId = moveTypes.First(y => y.Name == name).Id,
-                PokemonId = pokemonId
-            });
+                moveset.Add(new PokemonMoveset
+                {
+                    Id = id++,
+                    MoveId = moves.Any(y => y.Name.ToLower() == move.ToLower()) ? moves.First(y => y.Name.ToLower() == move.ToLower()).Id : 0,
+                    MoveTypeId = moveTypes.Any(y => y.Name == name) ? moveTypes.First(y => y.Name == name).Id : 0,
+                    PokemonId = pokemonId
+                });
+            }
+
+            return moveset;
         }
     }
 }
